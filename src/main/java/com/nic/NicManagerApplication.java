0c2a02d395b9ad6bbe9a143d15ca058f84d16335
wanglu1205 <1241943832@qq.com>
2019-10-17 11:11:40 +0800

    首次提交

---
+++ src/main/java/com/nic/NicManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicManagerApplication.class, args);
+    }
+
+}
